074031fdcdb68a4641725dec1ce5cb73e2955eea
M Ifraheem <mifrahim017@gmail.com>
2025-03-08 03:25:41 +0500

    env updated

--- api_gateway/main.go
+++ api_gateway/main.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 	"strings"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load .env file
+	if err := godotenv.Load("../.env"); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	http.HandleFunc("/", handleRequestAndRedirect)
 	log.Println("Server starting on port 6000...\n\n\thttp://localhost:6000/")
 	log.Fatal(http.ListenAndServe(":6000", nil))
@@ -22,10 +29,10 @@ func handleRequestAndRedirect(w http.ResponseWriter, r *http.Request) {
 }
 
 func getProxyURL(path string) *url.URL {
-	// Define your service URLs
-	authServiceURL, _ := url.Parse("http://localhost:8000/")
-	catalogServiceURL, _ := url.Parse("http://localhost:5000/")
-	orderServiceURL, _ := url.Parse("http://localhost:3000/")
+	// Retrieve service URLs from environment variables
+	authServiceURL, _ := url.Parse(os.Getenv("AUTH_SERVICE"))
+	catalogServiceURL, _ := url.Parse(os.Getenv("CATALOG_SERVICE"))
+	orderServiceURL, _ := url.Parse(os.Getenv("ORDER_SERVICE"))
 
 	switch {
 	case strings.HasPrefix(path, "/auth") || strings.HasPrefix(path, "/login") || strings.HasPrefix(path, "/register"):
