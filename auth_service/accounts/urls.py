9ec10649f9868bc8ebf4991ce9ce63f8a933f487
M Ifraheem <mifrahim017@gmail.com>
2025-03-08 01:22:08 +0500

    auth api's implemented

---
+++ auth_service/accounts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import UserRegistrationView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework_simplejwt.views import TokenVerifyView
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='user-register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+]
---
+++ auth_service/accounts/views.py
@@ -0,0 +1,11 @@
+from rest_framework import status, views
+from rest_framework.response import Response
+from .serializers import UserRegistrationSerializer
+
+class UserRegistrationView(views.APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
